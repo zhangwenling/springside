a7428fbffae2e02d66ea2bca50ee0a53b6bfc05f
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-03-19 10:11:16 +0000

    All in one版升级Maven2.0.10 和Nexus 1.3.1

--- springside3/trunk/bin/quickstart-all-in-one.bat
+++ springside3/trunk/bin/quickstart-all-in-one.bat
@@ -1,13 +1,16 @@
 @echo off
+echo [INFO] 确保默认JDK版本为JDK6.0.
+
 if exist "..\tools\maven" goto begin
 echo [INFO] tools\maven目录不存在，请下载all-in-one版本
 goto end
+
 :begin
-set MAVEN_BAT="%cd%\..\tools\maven\maven-2.0.9\bin\mvn.bat"
+set MAVEN_BAT="%cd%\..\tools\maven\maven-2.0.10\bin\mvn.bat"
 cd ..\
 
-echo [Step 1] 执行tools/maven/nexus/nexus-webapp-1.2.1/bin/jsw/windows-x86-32/Nexus.bat,启动Nexus Maven私服
-start tools\maven\nexus-1.2.1\nexus-webapp-1.2.1\bin\jsw\windows-x86-32\Nexus.bat
+echo [Step 1] 执行tools/maven/nexus/nexus-webapp/bin/jsw/windows-x86-32/Nexus.bat,启动Nexus Maven私服
+start tools\maven\nexus-1.3.1\nexus-webapp-1.3.1\bin\jsw\windows-x86-32\Nexus.bat
 
 echo [Step 2] 执行servers/derby/start-db.bat 以Standalone形式启动Derby数据库
 cd servers\derby
@@ -19,8 +22,8 @@ cd  servers\tomcat\apache-tomcat-6.0.18\bin\
 start startup.bat
 cd ..\..\..\..\
 
-echo 暂停20秒等待nexus启动
-ping -n 21 127.0.0.1>null
+echo [INFO] 暂停若干秒等待nexus启动.
+ping -n 16 127.0.0.1>null
 
 echo [Step 4] 安装SpringSide3 modules 和archetypes到 本地Maven仓库.
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=lib
@@ -31,7 +34,7 @@ cd examples\mini-web
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call %MAVEN_BAT% tomcat:undeploy
-call %MAVEN_BAT% clean compile war:exploded tomcat:exploded -Pinitdb
+call %MAVEN_BAT% clean compile war:exploded tomcat:exploded -Pinitdb -Pnotest
 cd ..\..\
 
 echo [Step 6] 初始化Jar包，编译、测试、打包、部署mini-service项目到tomcat，http://localhost:8080/mini-service
@@ -39,7 +42,7 @@ cd examples\mini-service
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call %MAVEN_BAT% tomcat:undeploy
-call %MAVEN_BAT% clean compile war:exploded tomcat:exploded
+call %MAVEN_BAT% clean compile war:exploded tomcat:exploded  -Pnotest
 cd ..\..\
 
 echo [Step 7] 初始化Jar包，编译、测试、打包、部署showcase项目到tomcat，http://localhost:8080/showcase
@@ -47,7 +50,7 @@ cd examples\showcase
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call %MAVEN_BAT% dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call %MAVEN_BAT% tomcat:undeploy
-call %MAVEN_BAT% clean compile war:exploded tomcat:exploded -Pinitdb
+call %MAVEN_BAT% clean compile war:exploded tomcat:exploded -Pinitdb  -Pnotest
 cd ..\..\
 
 echo [Step 8] 启动IE浏览上述三个项目 .
--- springside3/trunk/bin/quickstart.bat
+++ springside3/trunk/bin/quickstart.bat
@@ -1,4 +1,5 @@
 @echo off
+echo [INFO] 确保默认JDK版本为JDK6.0.
 echo [INFO] 确保设置PATH系统变量包含maven2.0.9或以上版本的bin目录.
 
 echo [Step 1] 安装SpringSide3 modules 和archetypes到本地Maven仓库.
@@ -15,7 +16,7 @@ echo 暂停5秒等待derby启动
 ping -n 6 127.0.0.1>null
 
 
-echo [Step 3] 请将servers/tomcat/ci-config-sample/conf/tomcat-user.xml 复制到你的tomcat目录，然后手工启动tomcat(端口为8080)，完成后请按任意键继续
+echo [Step 3] 请将servers/tomcat/ci-config/conf/tomcat-user.xml 复制到你的tomcat目录，然后手工启动tomcat(端口为8080)，完成后请按任意键继续
 pause
 
 
@@ -24,7 +25,7 @@ cd examples\mini-web
 call mvn dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call mvn dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call mvn tomcat:undeploy
-call mvn clean compile war:exploded tomcat:exploded -Pinitdb
+call mvn clean compile war:exploded tomcat:exploded -Pinitdb  -Pnotest
 cd ..\..\
 
 echo [Step 5] 初始化Jar包，编译、测试、打包、部署mini-service项目到tomcat，http://localhost:8080/mini-service
@@ -32,7 +33,7 @@ cd examples\mini-service
 call mvn dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call mvn dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call mvn tomcat:undeploy
-call mvn clean compile war:exploded tomcat:exploded
+call mvn clean compile war:exploded tomcat:exploded  -Pnotest
 cd ..\..\
 
 echo [Step 6] 初始化Jar包，编译、测试、打包、部署showcase项目到tomcat，http://localhost:8080/showcase
@@ -40,7 +41,7 @@ cd examples\showcase
 call mvn dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime
 call mvn dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib  -DincludeScope=runtime
 call mvn tomcat:undeploy
-call mvn clean compile war:exploded tomcat:exploded -Pinitdb
+call mvn clean compile war:exploded tomcat:exploded -Pinitdb  -Pnotest
 cd ..\..\
 
 echo [Step 7] 启动IE浏览上述三个项目 .
