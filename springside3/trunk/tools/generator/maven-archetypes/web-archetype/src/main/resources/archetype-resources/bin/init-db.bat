04953726dfe5936e383ff2785b4cc80329f0d655
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-04-12 08:26:54 +0000

    准备archetype

--- springside3/trunk/tools/generator/maven-archetypes/web-archetype/src/main/resources/archetype-resources/bin/init-db.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [INFO] [INFO] 使用maven sql plugin 初始化数据库.
-
-cd ..
-call mvn initialize -Pinitdb
-pause
--- springside3/trunk/tools/generator/maven-archetypes/web-archetype/src/main/resources/archetype-resources/bin/init-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "[INFO] 使用maven sql plugin 初始化数据库."
-
-cd ..
-mvn initialize -Pinitdb
