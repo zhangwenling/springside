fd2567094cb8db2ac9ede5d3e01bcc7fa830b582
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-05-13 12:30:44 +0000

    GmailEngine改进

--- springside3/trunk/modules/jee/src/main/java/org/springside/modules/security/jcaptcha/GMailEngine.java
+++ springside3/trunk/modules/jee/src/main/java/org/springside/modules/security/jcaptcha/GMailEngine.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.image.ImageFilter;
 
-import com.jhlabs.image.WaterFilter;
 import com.octo.captcha.component.image.backgroundgenerator.BackgroundGenerator;
 import com.octo.captcha.component.image.backgroundgenerator.UniColorBackgroundGenerator;
 import com.octo.captcha.component.image.color.RandomListColorGenerator;
@@ -29,36 +28,36 @@ import com.octo.captcha.engine.image.ListImageCaptchaEngine;
  * @author calvin
  */
 public class GMailEngine extends ListImageCaptchaEngine {
+	
+	private int minWordLength = 4;
+	private int maxWordLength = 4;
+	private int imageWidth = 200;
+	private int imageHeight = 70;
+	private int fontSize = 50;
 
 	@Override
 	protected void buildInitialFactories() {
 		//build filters
-		WaterFilter water = new WaterFilter(); //2.0 使用PinchFilter
-		water.setAmplitude(3d);
-		water.setAntialias(true);
-		water.setPhase(20d);
-		water.setWavelength(70d);
-		ImageDeformation postDef = new ImageDeformationByFilters(new ImageFilter[] { water });
+		ImageDeformation postDef = new ImageDeformationByFilters(new ImageFilter[] {});
 		ImageDeformation backDef = new ImageDeformationByFilters(new ImageFilter[] {});
 		ImageDeformation textDef = new ImageDeformationByFilters(new ImageFilter[] {});
 		
 		//word generator
 		WordGenerator dictionnaryWords = new ComposeDictionaryWordGenerator(new FileDictionary("toddlist"));
-		//wordtoimage components
-		TextPaster randomPaster = new DecoratedRandomTextPaster(7, 7, new RandomListColorGenerator(new Color[] {
+		
+		//word2image components
+		TextPaster randomPaster = new DecoratedRandomTextPaster(minWordLength, maxWordLength,
+				new RandomListColorGenerator(new Color[] {
 				new Color(23, 170, 27), new Color(220, 34, 11), new Color(23, 67, 172) }),
 				new TextDecorator[] { new BaffleTextDecorator(1, Color.white) });
 
-		BackgroundGenerator back = new UniColorBackgroundGenerator(200, 70, Color.white);
-
+	
 		FontGenerator shearedFont = new RandomFontGenerator(50, 50, new Font[] { new Font("nyala", Font.BOLD, 50),
 				new Font("Bell MT", Font.PLAIN, 50), new Font("Credit valley", Font.BOLD, 50) });
 		
-		//word2image 1
-		WordToImage word2image;
-		word2image = new DeformedComposedWordToImage(shearedFont, back, randomPaster, backDef, textDef, postDef);
+		BackgroundGenerator back = new UniColorBackgroundGenerator(200, 70, Color.white);
 
-		addFactory(new com.octo.captcha.image.gimpy.GimpyFactory(dictionnaryWords, word2image));
+		WordToImage word2image = new DeformedComposedWordToImage(shearedFont, back, randomPaster, backDef, textDef, postDef);
 
 		addFactory(new com.octo.captcha.image.gimpy.GimpyFactory(dictionnaryWords, word2image));
 	}
