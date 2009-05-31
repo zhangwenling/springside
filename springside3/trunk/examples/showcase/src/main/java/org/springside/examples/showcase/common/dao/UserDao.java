e5d10fe9a1df1e55b9cfc2cb9655c5ad0252d7cf
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-05-31 17:01:48 +0000

    Hibernate演示NativeSQL用法

--- springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/common/dao/UserDao.java
+++ springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/common/dao/UserDao.java
@@ -6,4 +6,16 @@ import org.springside.modules.orm.hibernate.HibernateDao;
 
 @Repository
 public class UserDao extends HibernateDao<User, Long> {
+	/**
+	 * hibernate native sql 演示.
+	 * 
+	 * 因为hibernate当前版本中SQLQueryReturnProcessor不能eager fetch many to many的bug, 因此仅加载User对象.
+	 */
+	public User findUserByNativeSQL(String loginName) {
+
+		return (User) getSession().createSQLQuery("select  {u.*} from USERS u  where u.LOGIN_NAME=:loginName")
+				.addEntity("u", User.class).setParameter("loginName", loginName).uniqueResult();
+
+	}
+
 }
