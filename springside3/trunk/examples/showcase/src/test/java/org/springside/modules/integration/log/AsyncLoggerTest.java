195da85118db86629b571353f95471ffe981477a
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-06-03 02:39:34 +0000

    Issue5: 升级Queue实现,增加性能测试用例

--- springside3/trunk/examples/showcase/src/test/java/org/springside/modules/integration/log/AsyncLoggerTest.java
+++ springside3/trunk/examples/showcase/src/test/java/org/springside/modules/integration/log/AsyncLoggerTest.java
@@ -1,5 +1,7 @@
 package org.springside.modules.integration.log;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +22,26 @@ public class AsyncLoggerTest extends SpringContextTestCase {
 		int logCount = 10;
 		for (int i = 1; i <= logCount; i++) {
 			dbLogger.info("helloworld {}", i);
+		}
+	}
+	
+	@Test
+	//@Ignore("Only for performanceTest")
+	public void performanceTest() throws InterruptedException {
+		Logger dbLogger = LoggerFactory.getLogger("org.springside.examples.showcase.log.dbLogExample");
+		int totalCount = 20000;
+		int tps = 4000;
+		Date batchBegin = new Date();
+		for (int i = 1; i <= totalCount; i++) {
+			dbLogger.info("helloworld {}", i);
+			if (i % tps == 0) {
+				Date batchEnd = new Date();
+				long sleepMillSecs = 1000 - (batchEnd.getTime() - batchBegin.getTime());
+				if (sleepMillSecs > 0) {
+					Thread.sleep(sleepMillSecs);
+				}
+				batchBegin = new Date();
+			}
 		}
 	}
 }
