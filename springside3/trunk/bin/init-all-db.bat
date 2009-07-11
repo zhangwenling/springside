2cdfef28336c33d843a72ebbe811b00f255faf4b
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-07-11 02:08:33 +0000

    Issue 24:  	 使用DBUnit管理测试与演示数据

--- springside3/trunk/bin/init-all-db.bat
+++ springside3/trunk/bin/init-all-db.bat
@@ -3,4 +3,14 @@ echo [INFO] 使用maven sql plugin 初始化所有Examples的数据库.
 
 cd ..\
 call mvn initialize -Pinitdb -Pexamples
+
+cd examples\mini-service
+call mvn dbunit:operation
+
+cd ..\mini-web
+call mvn dbunit:operation
+
+cd ..\showcase
+call mvn dbunit:operation
+
 pause
