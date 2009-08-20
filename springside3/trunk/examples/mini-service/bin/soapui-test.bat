52c281ec361d36df83d94191137d84369661f9a8
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-08-20 16:27:25 +0000

    支持使用jetty快速运行项目,使得功能测试更方便。

--- springside3/trunk/examples/mini-service/bin/soapui-test.bat
+++ springside3/trunk/examples/mini-service/bin/soapui-test.bat
@@ -1,7 +1,6 @@
 @echo off
 echo [INFO] 使用soapUi maven plugin 执行自动化测试.
-echo [INFO] 请确保Tomcat 6已启动并已在conf/tomcat-users.xml中设置admin用户.
 
 cd ..
-call mvn eviware:maven-soapui-plugin:test -Psoapui
+call mvn integration-test -Psoapui
 pause
