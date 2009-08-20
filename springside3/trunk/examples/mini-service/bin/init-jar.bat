4dfc1324052f0a32da3233fe0a104bd22b9437d4
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-08-20 13:19:22 +0000

    little fix the bat.

--- springside3/trunk/examples/mini-service/bin/init-jar.bat
+++ springside3/trunk/examples/mini-service/bin/init-jar.bat
@@ -1,5 +1,5 @@
 @echo off
-echo [INFO] 使用maven根据pom.xml 复制依赖jar到相应目录。
+echo [INFO] 使用maven根据pom.xml 复制依赖jar到相应目录.
 echo [INFO] compile,runtime级别到/webapp/WEB-INF/lib, test,provided级别到/lib
 
 set local_driver=%cd:~0,2%
--- springside3/trunk/examples/mini-web/bin/export-db.bat
+++ springside3/trunk/examples/mini-web/bin/export-db.bat
@@ -1,5 +1,5 @@
 @echo off
-echo [INFO] [INFO] 使用maven dbunit plugin 导出数据库.
+echo [INFO] 使用maven dbunit plugin 导出数据库.
 
 cd ..
 call mvn dbunit:export
