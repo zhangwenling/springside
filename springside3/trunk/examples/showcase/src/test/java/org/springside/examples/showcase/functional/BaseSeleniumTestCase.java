ad7d3a8b8ac2c34010c01919ea76a86621c1682c
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-08-21 17:04:58 +0000

    增加showcase中的selenium测试

--- springside3/trunk/examples/showcase/src/test/java/org/springside/examples/showcase/functional/BaseSeleniumTestCase.java
+++ springside3/trunk/examples/showcase/src/test/java/org/springside/examples/showcase/functional/BaseSeleniumTestCase.java
@@ -2,7 +2,7 @@ package org.springside.examples.showcase.functional;
 
 import org.springside.modules.test.selenium.SeleniumTestCase;
 
-public class BaseSeleniumTestCase extends SeleniumTestCase {
+public abstract class BaseSeleniumTestCase extends SeleniumTestCase {
 
 	protected void clickMenu(String menuName) {
 		selenium.open("/showcase");
