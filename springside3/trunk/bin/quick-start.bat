541a00fd7c4ce98f279dd82b8a8c234fb11f3d06
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-09-03 10:51:10 +0000

    集成cargo deploy tomcat的maven配置到根文件.

--- springside3/trunk/bin/quick-start.bat
+++ springside3/trunk/bin/quick-start.bat
@@ -12,37 +12,41 @@ cd ..\
 echo [Step 1] 复制tools/maven/central-repository 到 %userprofile%\.m2\repository
 xcopy /s/e/i/h/d/y "tools\maven\central-repository" "%USERPROFILE%\.m2\repository"
 
-echo [Step 2] 执行tools/derby/start-db.bat 以Standalone形式启动Derby数据库
+echo [Step 2] 执行tools/derby/start-db.bat 以Standalone形式启动Derby数据库.
 cd tools\derby
 start start-db.bat
 cd ..\..\
 
-echo [Step 3] 执行tools/tomcat/apache-tomcat-6.0.20/bin/startup.bat 启动Tomcat服务器
+echo [Step 3] 执行tools/tomcat/apache-tomcat-6.0.20/bin/startup.bat 启动Tomcat服务器.
 cd  tools\tomcat\apache-tomcat-6.0.20\bin\
 start startup.bat
 cd ..\..\..\..\
 
-echo [Step 4] 安装SpringSide3 modules 和archetypes到 本地Maven仓库.
+echo [Step 4] 为所有项目初始化依赖Jar包到lib及webapp/WEB-INF/lib目录.
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=lib -Dsilent=true -Pmodules
-call %MAVEN_BAT% -o clean install  -Pmodules -Pnotest
- 
-echo [Step 5] 为三个Example项目初始化依赖Jar包.
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime -Dsilent=true -Pexamples
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib -DincludeScope=runtime -Dsilent=true -Pexamples
 
-echo [Step 6] 为Mini-Service 初始化数据库，编译、测试、打包、部署到tomcat，并启动IE浏览。
+echo [Step 5] 安装SpringSide3 modules 和archetypes到 本地Maven仓库.
+call %MAVEN_BAT% -o clean install  -Pmodules -Pnotest
+ 
+echo [Step 6] 为Mini-Service 初始化数据库，编译、打包.
 cd examples\mini-service
-call %MAVEN_BAT% -o clean package cargo:redeploy -Pinitdb -Pnotest
-explorer http://localhost:8080/mini-service
+call %MAVEN_BAT% -o clean package -Pinitdb -Pnotest
 
-echo [Step 7] 为Mini-Web 初始化数据库，编译、测试、打包、部署到tomcat，并启动IE浏览。
+echo [Step 7] 为Mini-Web 初始化数据库，编译、打包.
 cd ..\mini-web
-call %MAVEN_BAT% -o clean package cargo:redeploy -Pinitdb -Pnotest
-explorer http://localhost:8080/mini-web
+call %MAVEN_BAT% -o clean package  -Pinitdb -Pnotest
 
-echo [Step 8] 为Showcase 初始化数据库，编译、测试、打包、部署到tomcat，并启动IE浏览。
+echo [Step 8] 为Showcase 初始化数据库，编译、打包.
 cd ..\showcase
-call %MAVEN_BAT% -o clean package cargo:redeploy -Pinitdb -Pnotest
+call %MAVEN_BAT% -o clean package  -Pinitdb -Pnotest
+
+echo [Step 9] 部署3个示例项目到tomcat，并启动浏览器浏览.
+cd ..\..
+call  %MAVEN_BAT% -o cargo:redeploy
+explorer http://localhost:8080/mini-service
+explorer http://localhost:8080/mini-web
 explorer http://localhost:8080/showcase
 
 echo [INFO] SpringSide3.0 快速启动完毕.
