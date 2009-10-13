ff8e7a669667b08e7147997ab082ee88e959ce80
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-10-13 17:07:07 +0000

    增加DES加解密功能的演示.

--- springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/SecurityUtils.java
+++ springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/SecurityUtils.java
@@ -11,53 +11,66 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
+/**
+ * 单向SHA-1散列与DES对称加密的Utils.
+ * 
+ * 使用Base64编码SHA1与DES产生的字节数组.
+ * 
+ * @author calvin
+ */
 public class SecurityUtils {
 
 	private static final String DES = "DES";
 
+	//-- SHA1 --//
 	/**
-	 * 对输入字符串进行Sha-1散列并进行Base64编码.
+	 * 对输入字符串进行sha1散列,并进行Hex编码.
 	 */
-	public static String sha1Base64(String input) {
-		byte[] digest = sha1(input);
-		return base64(digest);
-	}
-
-	/**
-	 * 对输入字符串进行Sha-1散列并进行Base64编码,编码时针对URL中的字符合法性进行特殊处理.
-	 */
-	public static String sha1Base64UrlSafe(String input) {
-		byte[] digest = sha1(input);
-		return base64UrlSafe(digest);
-	}
-
-	/**
-	 * 对输入字符串进行sha1散列.
-	 */
-	private static byte[] sha1(String input) {
+	public static String sha1(String input) {
 		MessageDigest messageDigest;
 		try {
 			messageDigest = MessageDigest.getInstance("SHA-1");
 		} catch (NoSuchAlgorithmException e) {
 			throw new IllegalStateException("Unexpected exception.", e);
 		}
-		return messageDigest.digest(input.getBytes());
+		byte[] digest = messageDigest.digest(input.getBytes());
+		return Hex.encodeHexString(digest);
 	}
 
+	//-- DES --//
+	/**
+	 * 使用DES加密原始字符串,返回Hex编码的结果.
+	 * 
+	 * @param input 原始输入字符串.
+	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 */
 	public static String desEncrypt(String input, String key) throws Exception {
 		byte[] encryptResult = des(input.getBytes(), key, Cipher.ENCRYPT_MODE);
-		return base64(encryptResult);
+		return Hex.encodeHexString(encryptResult);
 	}
 
+	/**
+	 * 使用DES解密Hex编码的加密字符串, 返回原始字符串.
+	 * 
+	 * @param input Hex编码的加密字符串.
+	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 */
 	public static String desDecrypt(String input, String key) throws Exception {
-		byte[] decryptResult = des(Base64.decodeBase64(input), key, Cipher.DECRYPT_MODE);
+		byte[] decryptResult = des(Hex.decodeHex(input.toCharArray()), key, Cipher.DECRYPT_MODE);
 		return new String(decryptResult);
 	}
 
+	/**
+	 * 使用DES加密或解密无编码的原始字节数组,返回无编码的字节数组结果.
+	 * 
+	 * @param input 无编码的原始字或加密字符串
+	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param mode Cipher.ENCRYPT_MODE 或 Cipher.DECRYPT_MODE
+	 */
 	private static byte[] des(byte[] input, String key, int mode) throws Exception {
-		DESKeySpec dks = new DESKeySpec(Base64.decodeBase64(key));
+		DESKeySpec dks = new DESKeySpec(Hex.decodeHex(key.toCharArray()));
 		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
 		Key k = keyFactory.generateSecret(dks);
 
@@ -66,26 +79,12 @@ public class SecurityUtils {
 		return cipher.doFinal(input);
 	}
 
-	public static String generateDesKey() throws Exception {
+	public static String desGenerateKey() throws Exception {
 		SecureRandom secureRandom = new SecureRandom();
 		KeyGenerator kg = KeyGenerator.getInstance(DES);
 		kg.init(secureRandom);
 		SecretKey secretKey = kg.generateKey();
-		return base64(secretKey.getEncoded());
+		return Hex.encodeHexString(secretKey.getEncoded());
 	}
 
-	/**
-	 * Base64编码.
-	 */
-	private static String base64(byte[] bytes) {
-		return new String(Base64.encodeBase64(bytes));
-	}
-
-	/**
-	 * Base64编码, 编码时针对URL中的字符合法性进行特殊处理.
-	 * eg. '+' -> '-' , '/' -> '_'
-	 */
-	private static String base64UrlSafe(byte[] bytes) {
-		return new String(Base64.encodeBase64URLSafe(bytes));
-	}
 }
--- springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/SecurityUtilsTest.java
+++ springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/SecurityUtilsTest.java
@@ -9,20 +9,19 @@ public class SecurityUtilsTest extends Assert {
 	@Test
 	public void digest() {
 		String input = "adkluqkddn";
-		System.out.println(input + " sha1 in base64 result         :" + SecurityUtils.sha1Base64(input));
-		System.out.println(input + " sha1 in base64 url safe result:" + SecurityUtils.sha1Base64UrlSafe(input));
+		System.out.println(input + " sha1 in hex result :" + SecurityUtils.sha1(input));
 	}
 
 	@Test
 	public void encrypt() throws Exception {
-		String key = SecurityUtils.generateDesKey();
-		System.out.println("key in base64:" + key);
+		String key = SecurityUtils.desGenerateKey();
+		System.out.println("des key in hex:" + key);
 
 		String input = "adfadfadca";
 		String encryptResult = SecurityUtils.desEncrypt(input, key);
 		String descryptResult = SecurityUtils.desDecrypt(encryptResult, key);
 
-		System.out.println(input + "des encrypt in base64 result:" + encryptResult);
+		System.out.println(input + "des encrypt in hex result:" + encryptResult);
 		assertEquals(input, descryptResult);
 	}
 }
