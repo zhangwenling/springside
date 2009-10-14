173769c8cc92b6992dd89c5e17a4af6961d129be
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-10-14 06:51:21 +0000

    增加DES加解密功能,sha1散列功能的演示.

--- springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/SecurityUtils.java
+++ springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/SecurityUtils.java
@@ -17,7 +17,7 @@ import org.apache.commons.codec.binary.Hex;
 /**
  * 单向SHA-1散列与DES对称加密的Utils.
  * 
- * 默认使用Hex编码字节数组, 亦可使用Base64版本的函数.
+ * 支持Hex与Base64两种编码方式.
  * 
  * @author calvin
  */
@@ -29,21 +29,24 @@ public class SecurityUtils {
 	/**
 	 * 对输入字符串进行sha1散列,并进行Hex编码.
 	 */
-	public static String sha1(String input) {
+	public static String sha1ToHex(String input) {
 
-		byte[] digest = sha1ToBytes(input);
+		byte[] digest = sha1(input);
 		return Hex.encodeHexString(digest);
 	}
 
 	/**
 	 * 对输入字符串进行sha1散列,并进行Base64编码.
 	 */
-	public static String sha1Base64(String input) {
-		byte[] digest = sha1ToBytes(input);
+	public static String sha1ToBase64(String input) {
+		byte[] digest = sha1(input);
 		return Base64.encodeBase64String(digest);
 	}
 
-	private static byte[] sha1ToBytes(String input) {
+	/**
+	 * 对输入字符串进行sha1散列,返回字节数组.
+	 */
+	private static byte[] sha1(String input) {
 		MessageDigest messageDigest;
 		try {
 			messageDigest = MessageDigest.getInstance("SHA-1");
@@ -55,24 +58,24 @@ public class SecurityUtils {
 
 	//-- DES --//
 	/**
-	 * 使用DES加密原始字符串,返回Hex编码的结果.
+	 * 使用DES加密原始字符串, 返回Hex编码的结果.
 	 * 
 	 * @param input 原始输入字符串.
-	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param hexKey 由desGenerateHexKey()生成的, 使用Hex编码的密钥.
 	 */
-	public static String desEncrypt(String input, String key) throws Exception {
-		byte[] encryptResult = des(input.getBytes(), key, Cipher.ENCRYPT_MODE);
+	public static String desEncryptToHex(String input, String hexKey) throws Exception {
+		byte[] encryptResult = des(input.getBytes(), hexKey, Cipher.ENCRYPT_MODE);
 		return Hex.encodeHexString(encryptResult);
 	}
 
 	/**
-	 * 使用DES加密原始字符串,返回Base64编码的结果.
+	 * 使用DES加密原始字符串, 返回Base64编码的结果.
 	 * 
 	 * @param input 原始输入字符串.
-	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param hexKey 由desGenerateHexKey()生成的, 使用Hex编码的密钥.
 	 */
-	public static String desEncryptBase64(String input, String key) throws Exception {
-		byte[] encryptResult = des(input.getBytes(), key, Cipher.ENCRYPT_MODE);
+	public static String desEncryptToBase64(String input, String hexKey) throws Exception {
+		byte[] encryptResult = des(input.getBytes(), hexKey, Cipher.ENCRYPT_MODE);
 		return Base64.encodeBase64String(encryptResult);
 	}
 
@@ -80,10 +83,10 @@ public class SecurityUtils {
 	 * 使用DES解密Hex编码的加密字符串, 返回原始字符串.
 	 * 
 	 * @param input Hex编码的加密字符串.
-	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param hexKey 由desGenerateHexKey()生成的, 使用Hex编码的密钥.
 	 */
-	public static String desDecrypt(String input, String key) throws Exception {
-		byte[] decryptResult = des(Hex.decodeHex(input.toCharArray()), key, Cipher.DECRYPT_MODE);
+	public static String desDecryptFromHex(String input, String hexKey) throws Exception {
+		byte[] decryptResult = des(Hex.decodeHex(input.toCharArray()), hexKey, Cipher.DECRYPT_MODE);
 		return new String(decryptResult);
 	}
 
@@ -91,29 +94,18 @@ public class SecurityUtils {
 	 * 使用DES解密Base64编码的加密字符串, 返回原始字符串.
 	 * 
 	 * @param input Base64编码的加密字符串.
-	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param hexKey 由desGenerateHexKey()生成的, 使用Hex编码的密钥.
 	 */
-	public static String desDecryptBase64(String input, String key) throws Exception {
-		byte[] decryptResult = des(Base64.decodeBase64(input), key, Cipher.DECRYPT_MODE);
+	public static String desDecryptFromBase64(String input, String hexKey) throws Exception {
+		byte[] decryptResult = des(Base64.decodeBase64(input), hexKey, Cipher.DECRYPT_MODE);
 		return new String(decryptResult);
 	}
 
 	/**
-	 * 生成符合DES规范的密钥,使用Hex编码.
-	 */
-	public static String desGenerateKey() throws Exception {
-		SecureRandom secureRandom = new SecureRandom();
-		KeyGenerator kg = KeyGenerator.getInstance(DES);
-		kg.init(secureRandom);
-		SecretKey secretKey = kg.generateKey();
-		return Hex.encodeHexString(secretKey.getEncoded());
-	}
-
-	/**
-	 * 使用DES加密或解密无编码的原始字节数组,返回无编码的字节数组结果.
+	 * 使用DES加密或解密无编码的原始字节数组, 返回无编码的字节数组结果.
 	 * 
 	 * @param input 无编码的原始字或加密字符串
-	 * @param key 由desGenerateKey()生成的, 使用Hex编码的密钥.
+	 * @param key 由desGenerateHexKey()生成的, 使用Hex编码的密钥.
 	 * @param mode Cipher.ENCRYPT_MODE 或 Cipher.DECRYPT_MODE
 	 */
 	private static byte[] des(byte[] input, String key, int mode) throws Exception {
@@ -124,5 +116,24 @@ public class SecurityUtils {
 		Cipher cipher = Cipher.getInstance(DES);
 		cipher.init(mode, k);
 		return cipher.doFinal(input);
+	}
+
+	/**
+	 * 生成符合DES规范的密钥, 返回Hex编码的结果.
+	 */
+	public static String desGenerateHexKey() throws Exception {
+		byte[] key = desGenerateKey();
+		return Hex.encodeHexString(key);
+	}
+
+	/**
+	 * 生成符合DES规范的密钥, 返回字节数组.
+	 */
+	private static byte[] desGenerateKey() throws Exception {
+		SecureRandom secureRandom = new SecureRandom();
+		KeyGenerator kg = KeyGenerator.getInstance(DES);
+		kg.init(secureRandom);
+		SecretKey secretKey = kg.generateKey();
+		return secretKey.getEncoded();
 	}
 }
--- springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/SecurityUtilsTest.java
+++ springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/SecurityUtilsTest.java
@@ -9,22 +9,22 @@ public class SecurityUtilsTest extends Assert {
 	@Test
 	public void digest() {
 		String input = "adkluqkddn";
-		System.out.println(input + " sha1 in hex result    :" + SecurityUtils.sha1(input));
-		System.out.println(input + " sha1 in base64 result :" + SecurityUtils.sha1Base64(input));
+		System.out.println(input + " sha1 in hex result    :" + SecurityUtils.sha1ToHex(input));
+		System.out.println(input + " sha1 in base64 result :" + SecurityUtils.sha1ToBase64(input));
 	}
 
 	@Test
 	public void encrypt() throws Exception {
-		String key = SecurityUtils.desGenerateKey();
+		String key = SecurityUtils.desGenerateHexKey();
 		System.out.println("des key in hex:" + key);
 
 		String input = "adfadfadca";
 
-		String encryptResult = SecurityUtils.desEncrypt(input, key);
-		String descryptResult = SecurityUtils.desDecrypt(encryptResult, key);
+		String encryptResult = SecurityUtils.desEncryptToHex(input, key);
+		String descryptResult = SecurityUtils.desDecryptFromHex(encryptResult, key);
 
-		String encryptBase64Result = SecurityUtils.desEncryptBase64(input, key);
-		String descryptBase64Result = SecurityUtils.desDecryptBase64(encryptBase64Result, key);
+		String encryptBase64Result = SecurityUtils.desEncryptToBase64(input, key);
+		String descryptBase64Result = SecurityUtils.desDecryptFromBase64(encryptBase64Result, key);
 
 		System.out.println(input + "des encrypt in hex result   :" + encryptResult);
 		System.out.println(input + "des encrypt in base64 result:" + encryptBase64Result);
