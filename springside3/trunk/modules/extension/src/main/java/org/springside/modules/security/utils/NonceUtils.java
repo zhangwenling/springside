c1b5fda7bd007e0ba16d91ab5dda398b3b3cddf7
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-10-15 08:17:54 +0000

    增加Nonce/timestamp utils

--- springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/NonceUtils.java
+++ springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/NonceUtils.java
@@ -1,25 +1,49 @@
 package org.springside.modules.security.utils;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class NonceUtils {
 
 	private static final SimpleDateFormat internateDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");//RFC3339
 
+	private static String previousTimestamp = "";
+	private static int timestampCounter = 0;
+
 	/**
 	 * 返回Internate标准格式的当前时间戳字符串.
 	 * 标准格式为yyyy-MM-dd'T'HH:mm:ss.SSS'Z', 如2009-10-15T14:24:50.316Z
 	 */
-	public static String getCurrentDate() {
+	public static String getCurrentTimestamp() {
 		Date now = new Date();
 		return internateDateFormat.format(now);
 	}
 
-	/**
-	 * 返回从1970到现在的毫秒数.
-	 */
-	public static String getCurrentTimpestamp() {
-		return Long.toString(new Date().getTime());
+	public synchronized static String nextTimestampNonce() {
+		String currentTimestamp = getCurrentTimestamp();
+
+		if (previousTimestamp.equals(currentTimestamp)) {
+			timestampCounter++;
+		} else {
+			previousTimestamp = currentTimestamp;
+			timestampCounter = 0;
+		}
+		return currentTimestamp + Integer.toString(timestampCounter);
+	}
+
+	public static String nextRandomNonce() {
+		try {
+			SecureRandom nonceGenerator = SecureRandom.getInstance("SHA1PRNG");
+			byte[] nonce = new byte[16];
+			nonceGenerator.nextBytes(nonce);
+			return CryptoUtils.hexEncode(nonce);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return UUID.randomUUID().toString();
 	}
 }
--- springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/NonceUtilsTest.java
+++ springside3/trunk/modules/extension/src/test/java/org/springside/modules/unit/security/utils/NonceUtilsTest.java
@@ -1,13 +1,56 @@
 package org.springside.modules.unit.security.utils;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.Test;
 import org.springside.modules.security.utils.NonceUtils;
 
 public class NonceUtilsTest {
 	@Test
 	public void timestamp() {
-		System.out.println(NonceUtils.getCurrentDate());
-		System.out.println(NonceUtils.getCurrentTimpestamp());
+		System.out.println("timestamp:" + NonceUtils.getCurrentTimestamp());
 	}
 
+	@Test
+	public void getRandomNonce() {
+		System.out.println("randomNonce   :" + NonceUtils.nextRandomNonce());
+	}
+
+	@Test
+	public void getTimestampNonce() {
+		System.out.println("timestampNonce:" + NonceUtils.nextTimestampNonce());
+	}
+
+	@Test
+	public void getRandomNoncesConcurrent() {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		for (int i = 0; i < 3; i++) {
+			executor.execute(new RandomNonceRequestTask());
+		}
+	}
+
+	@Test
+	public void getTimestampNoncesConcurrent() {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		for (int i = 0; i < 3; i++) {
+			executor.execute(new TimestampNonceRequestTask());
+		}
+	}
+
+	static class RandomNonceRequestTask implements Runnable {
+		public void run() {
+			for (int i = 0; i < 3; i++) {
+				System.out.println(NonceUtils.nextRandomNonce());
+			}
+		}
+	}
+
+	static class TimestampNonceRequestTask implements Runnable {
+		public void run() {
+			for (int i = 0; i < 3; i++) {
+				System.out.println(NonceUtils.nextTimestampNonce());
+			}
+		}
+	}
 }
