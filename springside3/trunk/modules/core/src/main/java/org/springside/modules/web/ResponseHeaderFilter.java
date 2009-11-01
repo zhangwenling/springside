a2f99e843f3afb51934593b0242f5658ac1e1331
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-11-01 16:25:58 +0000

    增加responseHeaderFilter

---
+++ springside3/trunk/modules/core/src/main/java/org/springside/modules/web/ResponseHeaderFilter.java
@@ -0,0 +1,44 @@
+package org.springside.modules.web;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 为Response设置Expires等Header的Filter.
+ * 
+ * @author calvin
+ */
+public class ResponseHeaderFilter implements Filter {
+
+	private FilterConfig fc;
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+			ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		// set the provided HTTP response parameters
+		for (Enumeration e = fc.getInitParameterNames(); e.hasMoreElements();) {
+			String headerName = (String) e.nextElement();
+			response.addHeader(headerName, fc.getInitParameter(headerName));
+		}
+
+		// pass the request/response on
+		chain.doFilter(req, response);
+	}
+
+	public void init(FilterConfig filterConfig) {
+		this.fc = filterConfig;
+	}
+
+	public void destroy() {
+		this.fc = null;
+	}
+}
