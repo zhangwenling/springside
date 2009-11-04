77e6cb0bd274da792487e0212943316ebe89d7d4
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-11-04 15:09:00 +0000

    增加sprignside-core测试覆盖率

--- springside3/trunk/modules/core/src/test/java/org/springside/modules/unit/spring/SpringContextHolderTest.java
+++ springside3/trunk/modules/core/src/test/java/org/springside/modules/unit/spring/SpringContextHolderTest.java
@@ -10,20 +10,20 @@ public class SpringContextHolderTest extends Assert {
 
 	@Test
 	public void testGetBean() {
-		
+
 		ReflectionUtils.setFieldValue(new SpringContextHolder(), "applicationContext", null);
-		
-		try{
-			SpringContextHolder.getBean("julToSlf4jHandler");	
+
+		try {
+			SpringContextHolder.getBean("julToSlf4jHandler");
 			fail("No exception throw for applicationContxt hadn't been init.");
-		}catch (IllegalStateException e){
-			
+		} catch (IllegalStateException e) {
+
 		}
-		
+
 		new ClassPathXmlApplicationContext("classpath:/applicationContext-test.xml");
 		assertNotNull(SpringContextHolder.getApplicationContext());
-		assertNotNull(SpringContextHolder.getBean("julToSlf4jHandler"));
+		assertNotNull(SpringContextHolder.getBean("springContextHolder"));
 		assertNotNull(SpringContextHolder.getBean(SpringContextHolder.class));
 	}
-	
+
 }
