3fd6797571aa9de23bb9377d8cff5a0a524786ec
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-12-11 16:58:44 +0000

    调整DigestUtils接口

--- springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/DigestUtils.java
+++ springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/DigestUtils.java
@@ -7,13 +7,15 @@
  */
 package org.springside.modules.security.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 
 import org.springside.modules.utils.EncodeUtils;
 
 /**
- * 支持SHA-1消息摘要的Util方法集合.
+ * 支持SHA-1/MD5消息摘要的Util方法集合.
  * 
  * 支持Hex与Base64两种编码方式.
  * 
@@ -22,13 +24,14 @@ import org.springside.modules.utils.EncodeUtils;
 public class DigestUtils {
 
 	private static final String SHA1 = "SHA-1";
+	private static final String MD5 = "MD5";
 
-	//-- SHA1 function --//
+	//-- String Hash function --//
 	/**
 	 * 对输入字符串进行sha1散列, 返回Hex编码的结果.
 	 */
 	public static String sha1ToHex(String input) {
-		byte[] digestResult = sha1(input);
+		byte[] digestResult = digest(input, SHA1);
 		return EncodeUtils.hexEncode(digestResult);
 	}
 
@@ -36,7 +39,7 @@ public class DigestUtils {
 	 * 对输入字符串进行sha1散列, 返回Base64编码的结果.
 	 */
 	public static String sha1ToBase64(String input) {
-		byte[] digestResult = sha1(input);
+		byte[] digestResult = digest(input, SHA1);
 		return EncodeUtils.base64Encode(digestResult);
 	}
 
@@ -44,24 +47,54 @@ public class DigestUtils {
 	 * 对输入字符串进行sha1散列, 返回Base64编码的URL安全的结果.
 	 */
 	public static String sha1ToBase64Url(String input) {
-		byte[] digestResult = sha1(input);
+		byte[] digestResult = digest(input, SHA1);
 		return EncodeUtils.base64UrlEncode(digestResult);
 	}
 
 	/**
-	 * 对输入字符串进行sha1散列, 返回字节数组.
+	 * 对字符串进行散列.
 	 */
-	private static byte[] sha1(String input) {
-		return sha1(input.getBytes());
+	private static byte[] digest(String input, String algorithm) {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+			return messageDigest.digest(input.getBytes());
+		} catch (GeneralSecurityException e) {
+			throw new IllegalStateException("Security exception", e);
+		}
+	}
+
+	//-- InputStream Hash function --//
+	/**
+	 * 对文件进行md5散列,返回Hex编码结果.
+	 */
+	public static String md5ToHex(InputStream input) throws IOException {
+		return digest(input, MD5);
 	}
 
 	/**
-	 * 对输入字节数组进行sha1散列, 返回字节数组.
+	 * 对文件进行sha1散列,返回Hex编码结果.
 	 */
-	private static byte[] sha1(byte[] input) {
+	public static String sha1ToHex(InputStream input) throws IOException {
+		return digest(input, SHA1);
+	}
+
+	/**
+	 * 对文件进行散列.
+	 */
+	private static String digest(InputStream input, String algorithm) throws IOException {
 		try {
-			MessageDigest messageDigest = MessageDigest.getInstance(SHA1);
-			return messageDigest.digest(input);
+			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+			int STREAM_BUFFER_LENGTH = 1024;
+			byte[] buffer = new byte[STREAM_BUFFER_LENGTH];
+			int read = input.read(buffer, 0, STREAM_BUFFER_LENGTH);
+
+			while (read > -1) {
+				messageDigest.update(buffer, 0, read);
+				read = input.read(buffer, 0, STREAM_BUFFER_LENGTH);
+			}
+
+			return EncodeUtils.hexEncode(messageDigest.digest());
+
 		} catch (GeneralSecurityException e) {
 			throw new IllegalStateException("Security exception", e);
 		}
