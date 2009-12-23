e02d98cbe756f96361c72adc9bbcae50ea7d67a9
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-12-23 15:12:15 +0000

    fix sonar

--- springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/DigestUtils.java
+++ springside3/trunk/modules/extension/src/main/java/org/springside/modules/security/utils/DigestUtils.java
@@ -84,13 +84,13 @@ public class DigestUtils {
 	private static String digest(InputStream input, String algorithm) throws IOException {
 		try {
 			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
-			int STREAM_BUFFER_LENGTH = 1024;
-			byte[] buffer = new byte[STREAM_BUFFER_LENGTH];
-			int read = input.read(buffer, 0, STREAM_BUFFER_LENGTH);
+			int bufferLength = 1024;
+			byte[] buffer = new byte[bufferLength];
+			int read = input.read(buffer, 0, bufferLength);
 
 			while (read > -1) {
 				messageDigest.update(buffer, 0, read);
-				read = input.read(buffer, 0, STREAM_BUFFER_LENGTH);
+				read = input.read(buffer, 0, bufferLength);
 			}
 
 			return EncodeUtils.hexEncode(messageDigest.digest());
