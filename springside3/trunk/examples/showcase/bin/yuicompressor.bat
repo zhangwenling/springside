527db4083b2c5efb5f9762b1fb07788959e2809b
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-12-25 14:04:03 +0000

    yuicompressor 升级2.4.2(自建私服)

--- springside3/trunk/examples/showcase/bin/yuicompressor.bat
+++ springside3/trunk/examples/showcase/bin/yuicompressor.bat
@@ -1,5 +1,5 @@
-java -jar ..\lib\yuicompressor-2.3.6.jar -o ..\webapp\js\showcase\shape.js ..\webapp\js\showcase\shape-src.js
-java -jar ..\lib\yuicompressor-2.3.6.jar -o ..\webapp\css\yui.css ..\webapp\css\yui-src.css
-java -jar ..\lib\yuicompressor-2.3.6.jar -o ..\webapp\css\style.css ..\webapp\css\style-src.css
+java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\js\showcase\shape.js ..\webapp\js\showcase\shape-src.js
+java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\css\yui.css ..\webapp\css\yui-src.css
+java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\css\style.css ..\webapp\css\style-src.css
 
 pause
