0d2823701c3db62b2851c7384908e69afbcd8eba
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2009-12-28 07:43:53 +0000

    修改cargo插件, 改为本地部署war包.

--- springside3/trunk/bin/quick-start.bat
+++ springside3/trunk/bin/quick-start.bat
@@ -17,39 +17,35 @@ cd tools\h2
 call h2w.bat
 cd ..\..\
 
-echo [Step 3] 执行tools/tomcat/apache-tomcat-6.0.20/bin/startup.bat 启动Tomcat服务器.
-cd  tools\tomcat\apache-tomcat-6.0.20\bin\
-start startup.bat
-cd ..\..\..\..\
+echo [Step 3] 安装SpringSide3 modules 和archetypes到 本地Maven仓库.
+call %MAVEN_BAT% -o clean install  -Pmodules -Dmaven.test.skip=true
 
-echo [Step 4] 为所有项目初始化依赖Jar包到lib及webapp/WEB-INF/lib目录.
+echo [Step 4] 为Mini-Service 初始化数据库、编译、打包.
+cd examples\mini-service
+call %MAVEN_BAT% -o clean package -Pinitdb -Dmaven.test.skip=true
+
+echo [Step 5] 为Mini-Web 初始化数据库、编译、打包.
+cd ..\mini-web
+call %MAVEN_BAT% -o clean package -Pinitdb -Dmaven.test.skip=true
+
+echo [Step 6] 为Showcase 初始化数据库、编译、打包.
+cd ..\showcase
+call %MAVEN_BAT% -o clean package -Pinitdb -Dmaven.test.skip=true
+
+echo [Step 7] 部署3个示例项目到tomcat，启动tomcat.
+cd ..\..
+start "Tomcat Server" "%MAVEN_BAT%" cargo:deploy cargo:start
+
+echo [Step 8] 为所有项目初始化依赖Jar包到lib及webapp/WEB-INF/lib目录,方便浏览
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=lib -Dsilent=true -Pmodules
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime -Dsilent=true -Pexamples
 call %MAVEN_BAT% -o dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib -DincludeScope=runtime -Dsilent=true -Pexamples
 
-echo [Step 5] 安装SpringSide3 modules 和archetypes到 本地Maven仓库.
-call %MAVEN_BAT% -o clean install  -Pmodules -Dmaven.test.skip=true
- 
-echo [Step 6] 为Mini-Service 初始化数据库、编译、打包.
-cd examples\mini-service
-call %MAVEN_BAT% -o clean package -Pinitdb -Dmaven.test.skip=true
-
-echo [Step 7] 为Mini-Web 初始化数据库、编译、打包.
-cd ..\mini-web
-call %MAVEN_BAT% -o clean package  -Pinitdb -Dmaven.test.skip=true
-
-echo [Step 8] 为Showcase 初始化数据库、编译、打包.
-cd ..\showcase
-call %MAVEN_BAT% -o clean package  -Pinitdb -Dmaven.test.skip=true
-
-echo [Step 9] 部署3个示例项目到tomcat，并启动浏览器浏览.
-cd ..\..
-call  %MAVEN_BAT% -o cargo:redeploy
-explorer http://localhost:8080/mini-service
-explorer http://localhost:8080/mini-web
-explorer http://localhost:8080/showcase
-
 echo [INFO] SpringSide3.0 快速启动完毕.
+echo [INFO] 可使用浏览器访问以下网址.
+echo [INFO] http://localhost:8080/mini-service
+echo [INFO] http://localhost:8080/mini-web
+echo [INFO] http://localhost:8080/showcase
 :end
 pause
 
