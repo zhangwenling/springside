48f5f3e3fccdf9728fc48f797e72d22b6b483a19
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-01-18 17:16:13 +0000

    规范maven使用

--- springside3/trunk/examples/mini-web/bin/eclipse.bat
+++ springside3/trunk/examples/mini-web/bin/eclipse.bat
@@ -2,5 +2,5 @@
 echo [INFO] 使用maven eclipse plugin 下载所需jar并生成Eclipse项目文件.
 
 cd ..
-call mvn eclipse:eclipse
+call mvn eclipse:clean eclipse:eclipse
 pause
--- springside3/trunk/modules/core/eclipse.bat
+++ springside3/trunk/modules/core/eclipse.bat
@@ -1,5 +1,5 @@
 @echo off
 echo [INFO] 使用maven eclipse plugin 下载所需jar并生成Eclipse项目文件.
 
-call mvn eclipse:eclipse
+call mvn eclipse:clean eclipse:eclipse
 pause
