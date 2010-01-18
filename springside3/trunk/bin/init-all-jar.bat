115ba8f8a3a1a91d57c90fc2e1027f4a1926a9d5
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-01-18 11:40:25 +0000

    改进Maven用法, mvn eclipse:eclipse 不错

--- springside3/trunk/bin/init-all-jar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo [INFO] 从maven仓库复制jar到所有项目目录.
-
-cd ..\
-call mvn  dependency:copy-dependencies -DoutputDirectory=lib -Dsilent=true -Pmodules
-call mvn  dependency:copy-dependencies -DoutputDirectory=lib -DexcludeScope=runtime -Dsilent=true -Pexamples
-call mvn  dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib -DincludeScope=runtime -Dsilent=true -Pexamples
-
-pause
