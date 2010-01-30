312f039d2f54bae55da2a0e791251ed1829ad3a2
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-01-30 03:59:26 +0000

    improve

--- springside3/trunk/examples/mini-service/bin/db-export.bat
+++ springside3/trunk/examples/mini-service/bin/db-export.bat
@@ -2,5 +2,5 @@
 echo [INFO] 使用dbunit导出测试数据.
 
 cd ..
-call mvn db:export
+call mvn dbunit:export
 pause
---
+++ springside3/trunk/modules/core/src/main/java/org/springside/modules/orm/hibernate/IgnorePrefixReverseEngineeringStrategy.java
@@ -0,0 +1,6 @@
+package org.springside.modules.orm.hibernate;
+
+public class IgnorePrefixReverseEngineeringStrategy   extends DelegatingReverseEngineeringStrategy {
+	
+
+}
