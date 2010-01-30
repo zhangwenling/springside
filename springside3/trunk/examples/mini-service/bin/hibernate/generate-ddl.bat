49af4d71a62121843bab8755bd3d3847fdd8ceac
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-01-30 03:27:03 +0000

    refactor database table name

--- springside3/trunk/examples/mini-service/bin/hibernate/generate-ddl.bat
+++ springside3/trunk/examples/mini-service/bin/hibernate/generate-ddl.bat
@@ -1,5 +1,6 @@
 @echo off
 echo [INFO] Generate database schema from entity defined to %cd%\generated dir.
+echo [INFO] Make sure maven's ant task jar in ant's class path.
 call ant generate.ddl
 echo [INFO] Artifact will generate  in %cd%\generated dir.
 pause
