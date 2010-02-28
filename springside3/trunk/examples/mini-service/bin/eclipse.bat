8ab6d9ba504eb14ccf1f4e0fc254580280f7606c
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-02-28 16:10:45 +0000

    little fixing

--- springside3/trunk/examples/mini-service/bin/eclipse.bat
+++ springside3/trunk/examples/mini-service/bin/eclipse.bat
@@ -1,6 +1,6 @@
 @echo off
 echo [INFO] Use maven eclipse plugin download jar and generate eclipse project files.
-echo [Info] If your eclipse files not in default place, add -Declipse.workspace=<path-to-eclipse-workspace> 
+echo [Info] Add "-Declipse.workspace=<path-to-eclipse-workspace>"
 
 cd ..
 call mvn eclipse:clean eclipse:eclipse
--- springside3/trunk/modules/core/eclipse.bat
+++ springside3/trunk/modules/core/eclipse.bat
@@ -1,6 +1,7 @@
 @echo off
 echo [INFO] Use maven eclipse plugin download jar and generate eclipse project files.
-echo [Info] If your eclipse files not in default place, add -Declipse.workspace=<path-to-eclipse-workspace> 
+echo [INFO] Add "-Declipse.workspace=<path-to-eclipse-workspace>"
 
 call mvn eclipse:clean eclipse:eclipse
+
 pause
