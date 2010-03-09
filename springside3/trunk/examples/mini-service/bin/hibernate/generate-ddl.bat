60f555331c6b3570485a5671aaaab7248ecb8dd2
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-03-09 07:51:32 +0000

    更新Hibernate Entity生成工具

--- springside3/trunk/examples/mini-service/bin/hibernate/generate-ddl.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [INFO] Generate database schema from entity defined to %cd%\generated dir.
-echo [INFO] Make sure maven's ant task jar in ant's classpath.
-call ant generate.ddl
-echo [INFO] Artifact will generate  in %cd%\generated dir.
-pause
--- springside3/trunk/modules/core/src/main/java/org/springside/modules/orm/hibernate/IgnorePrefixReverseEngineeringStrategy.java
+++ springside3/trunk/modules/core/src/main/java/org/springside/modules/orm/hibernate/IgnorePrefixReverseEngineeringStrategy.java
@@ -1,20 +1,16 @@
 package org.springside.modules.orm.hibernate;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy;
 import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
 import org.hibernate.cfg.reveng.TableIdentifier;
 
 /**
  * Hibernate Tools 从数据库逆向生成Entity POJO时, Class名要去除表名中的前缀的策略.
- * @author ehuaxio
- *
+ * 继承子类修改前缀长度.
+ * 
+ * @author calvin
  */
 public class IgnorePrefixReverseEngineeringStrategy extends DelegatingReverseEngineeringStrategy {
-
-	private Set<String> printed = new HashSet<String>();
 
 	public IgnorePrefixReverseEngineeringStrategy(ReverseEngineeringStrategy delegate) {
 		super(delegate);
@@ -24,17 +20,17 @@ public class IgnorePrefixReverseEngineeringStrategy extends DelegatingReverseEng
 	public String tableToClassName(TableIdentifier tableIdentifier) {
 		String delegateResult = super.tableToClassName(tableIdentifier);
 		int index = delegateResult.lastIndexOf('.');
-		String className = delegateResult.substring(0, index + 1)
-				+ delegateResult.substring(index + getPrefixLength() + 1);
-
-		if (!printed.contains(className)) {
-			System.out.println("<mapping class=\"" + className + "\" >");
-			printed.add(className);
-		}
-
-		return className;
+		
+		String packageName = delegateResult.substring(0, index + 1);
+		String className = delegateResult.substring(index + getPrefixLength() + 1);
+		String fullClassName = packageName + className;
+		
+		return fullClassName;
 	}
 
+	/**
+	 * 可在子类重载的长度.
+	 */
 	protected int getPrefixLength() {
 		return 4;
 	}
