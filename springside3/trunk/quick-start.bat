f9e8dbcf12e6ae1a6aca5c6fc988e994d5ea092a
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-04-17 05:57:30 +0000

    升级3.3.1, 发行包的fixing

--- springside3/trunk/quick-start.bat
+++ springside3/trunk/quick-start.bat
@@ -1,38 +1,44 @@
 @echo off
 echo [INFO] 确保默认JDK版本为JDK5.0及以上版本.
-echo [INFO] 确保Maven的可执行命令在PATH内.
-echo [INFO] 确保Ant的可执行命令在PATH内, 并已将maven-ant-task.jar放入Ant的lib中.
 
+rem [INFO]使用ONLINE模式执行Maven命令, 如不能连接网络, 去掉下面一行的注释.
 rem set OFF_LINE=-o
+
+echo [INFO] All in one版本使用自带的Maven与Ant.
+set MVN=mvn
+set ANT=ant
+
+if exist "..\tools\maven\apache-maven-2.2.1\" set MVN="%cd%\tools\maven\apache-maven-2.2.1\bin\mvn.bat"
+if exist "..\tools\ant\aapache-ant-1.7.1\" set ANT="%cd%\tools\ant\apache-ant-1.7.1\bin\ant.bat"
 
 echo [Step 1] 复制tools/maven/central-repository 到 %userprofile%\.m2\repository
 xcopy /s/e/i/h/d/y "tools\maven\central-repository" "%USERPROFILE%\.m2\repository"
 
 echo [Step 2] 启动H2数据库.
 cd tools/h2
-call mvn %OFF_LINE% initialize -Pstartdb
+call %MVN% %OFF_LINE% initialize -Pstartdb
 cd ..\..\
 
 echo [Step 3] 安装SpringSide3 所有modules,examples项目到本地Maven仓库,生成Eclipse项目文件.
-call mvn %OFF_LINE% eclipse:clean eclipse:eclipse
-call mvn %OFF_LINE% clean install -Dmaven.test.skip=true
+call %MVN% %OFF_LINE% eclipse:clean eclipse:eclipse
+call %MVN% %OFF_LINE% clean install -Dmaven.test.skip=true
 
 echo [Step 4] 为Mini-Service 初始化数据库, 启动Jetty.
 cd examples\mini-service
-call ant init-db 
-start "Mini-Service" mvn %OFF_LINE% -Djetty.port=8080 jetty:run
+call %ANT% -f bin/build.xml init-db 
+start "Mini-Service" %MVN% %OFF_LINE% -Djetty.port=8080 jetty:run
 cd ..\..\
 
 echo [Step 5] 为Mini-Web 初始化数据库, 启动Jetty.
 cd examples\mini-web
-call ant init-db 
-start "Mini-Web" mvn %OFF_LINE% -Djetty.port=8082 jetty:run
+call %ANT% -f bin/build.xml init-db 
+start "Mini-Web" %MVN% %OFF_LINE% -Djetty.port=8082 jetty:run
 cd ..\..\
 
 echo [Step 6] 为Showcase 生成Eclipse项目文件, 编译, 打包, 初始化数据库, 启动Jetty.
 cd examples\showcase
-call ant init-db
-start "Showcase" mvn %OFF_LINE% -Djetty.port=8083 jetty:run
+call %ANT% -f bin/build.xml init-db
+start "Showcase" %MVN% %OFF_LINE% -Djetty.port=8083 jetty:run
 cd ..\..\
 
 echo [INFO] SpringSide3.0 快速启动完毕.
