5dfa0acd72443a7bbe26ea5ab9665db4f34bc245
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-04-19 04:15:18 +0000

    littlefix

--- springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/web/CacheControlHeaderFilter.java
+++ springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/web/CacheControlHeaderFilter.java
@@ -13,12 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springside.modules.web.ServletUtils;
 
 /**
- * 为Response设置Expires等Header的Filter.
+ * 为Response设置客户端缓存控制Header的Filter.
  * 
  * eg.在web.xml中设置
  * 	<filter>
  * 		<filter-name>cacheControlHeaderFilter</filter-name>
- * 		<filter-class>org.springside.modules.web.ResponseHeaderFilter</filter-class>
+ * 		<filter-class>org.springside.modules.web.CacheControlHeaderFilter</filter-class>
  * 		<init-param>
  * 			<param-name>expiresSeconds</param-name>
  * 			<param-value>31536000</param-value>
@@ -33,13 +33,12 @@ import org.springside.modules.web.ServletUtils;
  */
 public class CacheControlHeaderFilter implements Filter {
 
-	private static final String PARAM_EXPIRES_SECOND = "expiresSeconds";
+	private static final String PARAM_EXPIRES_SECONDS = "expiresSeconds";
 	private long expiresSeconds;
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
 			ServletException {
 		ServletUtils.setExpiresHeader((HttpServletResponse) res, expiresSeconds);
-		// pass the request/response on
 		chain.doFilter(req, res);
 	}
 
@@ -47,7 +46,7 @@ public class CacheControlHeaderFilter implements Filter {
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig filterConfig) {
-		String expiresSecondsParam = filterConfig.getInitParameter(PARAM_EXPIRES_SECOND);
+		String expiresSecondsParam = filterConfig.getInitParameter(PARAM_EXPIRES_SECONDS);
 		if (expiresSecondsParam != null) {
 			expiresSeconds = Long.valueOf(expiresSecondsParam);
 		} else {
@@ -59,12 +58,5 @@ public class CacheControlHeaderFilter implements Filter {
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-	}
-	
-	public static void setExpiresHeader(HttpServletResponse response, long expiresSeconds) {
-		//Http 1.0 header
-		response.setDateHeader("Expires", System.currentTimeMillis() + expiresSeconds * 1000);
-		//Http 1.1 header
-		response.setHeader("Cache-Control", "private, max-age=" + expiresSeconds);
 	}
 }
