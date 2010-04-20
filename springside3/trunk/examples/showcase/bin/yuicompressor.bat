582fdeaa6fb62c91630c991fa949ecd26a0a8567
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-04-20 14:12:27 +0000

    bug fixing

--- springside3/trunk/examples/showcase/bin/yuicompressor.bat
+++ springside3/trunk/examples/showcase/bin/yuicompressor.bat
@@ -1,5 +1,6 @@
-java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\js\showcase\shape.js ..\webapp\js\showcase\shape-src.js
-java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\css\yui.css ..\webapp\css\yui-src.css
-java -jar ..\lib\yuicompressor-2.4.2.jar -o ..\webapp\css\style.css ..\webapp\css\style-src.css
+echo please manual copy yuicompressor-2.4.2.jar from maven repository to here
+java -jar yuicompressor-2.4.2.jar ..\src\main\webapp\js\showcase\shape-src.js -o ..\src\main\webapp\js\showcase\shape.js 
+java -jar yuicompressor-2.4.2.jar ..\src\main\webapp\css\yui-src.css -o ..\src\main\webapp\css\yui.css 
+java -jar yuicompressor-2.4.2.jar ..\src\main\webapp\css\style-src.css -o ..\src\main\webapp\css\style.css 
 
 pause
