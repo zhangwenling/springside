811f361a9418ebc6dbde9c8c9180599d0dd30587
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-04-27 17:25:45 +0000

    参考Hibernate ShardId，编写了一个生成16个字节长ID，支持多数据服务器多Hibernate应用的IdGenerator.

--- springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/common/entity/IdEntity.java
+++ springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/common/entity/IdEntity.java
@@ -1,9 +1,10 @@
 package org.springside.examples.showcase.common.entity;
 
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * 统一定义id的entity基类.
@@ -20,7 +21,8 @@ public abstract class IdEntity {
 	protected Long id;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "shardIdGenerator")
+	@GenericGenerator(name = "shardIdGenerator", strategy = "org.springside.modules.orm.hibernate.ShardIdGenerator")
 	public Long getId() {
 		return id;
 	}
