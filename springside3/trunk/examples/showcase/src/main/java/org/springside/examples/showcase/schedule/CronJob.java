c711c1fe1be63be930935adb5a7ef4ab73579233
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-07-30 15:48:16 +0000

    升级Scheduler演示，取消Spring对JDK ScheduleExecutor的封装，增加Spring3.0的Cron式Executor的演示.

--- springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/schedule/CronJob.java
+++ springside3/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/schedule/CronJob.java
@@ -1,6 +1,5 @@
 package org.springside.examples.showcase.schedule;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -10,42 +9,42 @@ import javax.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.support.DelegatingErrorHandlingRunnable;
-import org.springframework.scheduling.support.TaskUtils;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.util.Assert;
 import org.springside.examples.showcase.common.service.AccountManager;
 
 /**
- * 被ScheduledThreadPoolExecutor定时执行的任务类.
+ * 使用Spring的ThreadPoolTaskScheduler执行Cron式任务的类.
  */
-public class ExecutorJob implements Runnable {
+public class CronJob implements Runnable {
 
-	private static Logger logger = LoggerFactory.getLogger(ExecutorJob.class);
+	private static Logger logger = LoggerFactory.getLogger(CronJob.class);
 
-	private int initialDelay = 0;
-
-	private int period = 0;
+	private String cronExpression;
 
 	private long shutdownTimeout = 0;
 
-	private ScheduledExecutorService scheduledExecutorService;
+	private ThreadPoolTaskScheduler threadPoolTaskScheduler;
 
 	private AccountManager accountManager;
 
 	@PostConstruct
-	public void start() throws Exception {
-		Assert.isTrue(period > 0);
+	public void start() {
+		Assert.hasText(cronExpression);
 
-		//任何异常不会中断执行schedule执行
-		Runnable task = new DelegatingErrorHandlingRunnable(this, TaskUtils.LOG_AND_SUPPRESS_ERROR_HANDLER);
+		threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.initialize();
 
-		scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-		scheduledExecutorService.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+		threadPoolTaskScheduler.schedule(this, new CronTrigger(cronExpression));
 	}
 
 	@PreDestroy
 	public void stop() {
+		ScheduledExecutorService scheduledExecutorService = threadPoolTaskScheduler.getScheduledExecutor();
+
 		scheduledExecutorService.shutdownNow();
+
 		if (shutdownTimeout > 0) {
 			try {
 				scheduledExecutorService.awaitTermination(shutdownTimeout, TimeUnit.SECONDS);
@@ -62,12 +61,8 @@ public class ExecutorJob implements Runnable {
 		logger.info("There are {} user in database.", userCount);
 	}
 
-	public void setInitialDelay(int initialDelay) {
-		this.initialDelay = initialDelay;
-	}
-
-	public void setPeriod(int period) {
-		this.period = period;
+	public void setCronExpression(String cronExpression) {
+		this.cronExpression = cronExpression;
 	}
 
 	public void setShutdownTimeout(long shutdownTimeout) {
