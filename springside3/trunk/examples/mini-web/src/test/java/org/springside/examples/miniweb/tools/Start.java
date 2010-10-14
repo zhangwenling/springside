a10d9fe6f47bf533b7f66cef5b5178a93ac76686
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-10-14 06:59:03 +0000

    fix port to run in hudson

--- springside3/trunk/examples/mini-web/src/test/java/org/springside/examples/miniweb/tools/Start.java
+++ springside3/trunk/examples/mini-web/src/test/java/org/springside/examples/miniweb/tools/Start.java
@@ -10,9 +10,9 @@ import org.springside.modules.test.utils.JettyUtils;
  */
 public class Start {
 
-	public static final int PORT = 8080;
+	public static final int PORT = 9090;
 	public static final String CONTEXT = "/mini-web";
-	public static final String BASE_URL = "http://localhost:8080/mini-web";
+	public static final String BASE_URL = "http://localhost:9090/mini-web";
 
 	public static void main(String[] args) throws Exception {
 		Server server = JettyUtils.buildNormalServer(PORT, CONTEXT);
