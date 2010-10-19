94d82472ac1a7927234387980cfad78911cbe610
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-10-19 16:58:06 +0000

    assembly打包方式改进

--- springside3/trunk/examples/showcase/bin/package.bat
+++ springside3/trunk/examples/showcase/bin/package.bat
@@ -3,6 +3,6 @@ echo [INFO] Use maven assembly-plugin package jsw+jetty+webapp zip.
 
 cd %~dp0
 cd ..
-call mvn  package -Dpackage.classifier=test -Dmaven.test.skip=true
+call mvn -U clean package -Dpackage.bin=true -Dmaven.test.skip=true
 cd bin
 pause 
