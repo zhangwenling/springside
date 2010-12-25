3818bded26ae7b641e9983330b3d842866967e74
badqiu <badqiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-12-25 14:11:27 +0000

    add ValidatorHolder

---
+++ springside3/trunk/modules/core/src/main/java/org/springside/modules/utils/validator/ValidatorHolder.java
@@ -0,0 +1,69 @@
+package org.springside.modules.utils.validator;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import javax.validation.metadata.BeanDescriptor;
+
+import org.springframework.beans.factory.InitializingBean;
+/**
+ * 用于持有JSR303 Validator(Hibernate Validator),使调用Validator可以当静态方法使用.
+ * 
+ * <pre>
+ * spring配置:
+ * &lt;bean class="org.springside.modules.utils.validator.ValidatorHolder">
+ * 	 &lt;preperty name="validator" ref="validator"/>
+ * &lt;/bean>
+ * </pre> 
+ * @author badqiu
+ *
+ */
+public class ValidatorHolder implements InitializingBean{
+	private static Validator validator;
+
+	public void afterPropertiesSet() throws Exception {
+		if(validator == null) throw new IllegalStateException("not found JSR303(HibernateValidator) 'validator' for ValidatorHolder ");
+	}
+	
+	public void setValidator(Validator v) {
+		if(ValidatorHolder.validator != null) {
+			throw new IllegalStateException("ValidatorHolder already holded 'validator'");
+		}
+		ValidatorHolder.validator = v;
+	}
+
+	public static Validator getValidator() {
+		if(validator == null)
+			throw new IllegalStateException("'validator' property is null,ValidatorHolder not yet init.");
+		return validator;
+	}
+
+	public <T> Set<ConstraintViolation<T>> validate(T object,
+			Class<?>... groups) {
+		return getValidator().validate(object, groups);
+	}
+
+	public <T> Set<ConstraintViolation<T>> validateProperty(T object,
+			String propertyName, Class<?>... groups) {
+		return getValidator().validateProperty(object, propertyName, groups);
+	}
+
+	public <T> Set<ConstraintViolation<T>> validateValue(Class<T> beanType,
+			String propertyName, Object value, Class<?>... groups) {
+		return getValidator().validateValue(beanType, propertyName, value, groups);
+	}
+
+	public static final BeanDescriptor getConstraintsForClass(Class<?> clazz) {
+		return getValidator().getConstraintsForClass(clazz);
+	}
+	
+	public static final <T> T unwrap(Class<T> type) {
+		return getValidator().unwrap(type);
+	}
+	
+	public static void cleanHolder() {
+		validator = null;
+	}
+}
