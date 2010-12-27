f264c0c0048b283dd4ceb4ffbe45586a50a6dd5d
badqiu <badqiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2010-12-27 17:44:21 +0000

    增加标注

--- springside3/trunk/examples/mini-service/src/main/java/org/springside/examples/miniservice/ws/AccountWebService.java
+++ springside3/trunk/examples/mini-service/src/main/java/org/springside/examples/miniservice/ws/AccountWebService.java
@@ -30,7 +30,7 @@ public interface AccountWebService {
 	 * 搜索用户信息.
 	 */
 	UserPageResult searchUser(@WebParam(name = "loginName") String loginName,
-			@WebParam(name = "name") String name,int pageNo,int pageSize);
+			@WebParam(name = "name") String name,@WebParam(name = "pageNo") int pageNo,@WebParam(name = "pageSize")int pageSize);
 
 	/**
 	 * 新建用户.
