3657e7fc54ef0ff46145ba6d9cfbb173fef77341
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2011-05-09 13:47:55 +0000

    小重构ConvertUtils

---
+++ springside4/trunk/modules/core/src/test/java/org/springside/modules/unit/utils/CollecitonUtilsTest.java
@@ -0,0 +1,40 @@
+package org.springside.modules.unit.utils;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springside.modules.unit.utils.ReflectionUtilsTest.TestBean3;
+import org.springside.modules.utils.CollectionUtils;
+
+import com.google.common.collect.Lists;
+
+public class CollecitonUtilsTest {
+
+	@Test
+	public void convertElementPropertyToString() {
+		TestBean3 bean1 = new TestBean3();
+		bean1.setId(1);
+		TestBean3 bean2 = new TestBean3();
+		bean2.setId(2);
+
+		List list = Lists.newArrayList(bean1, bean2);
+
+		assertEquals("1,2", CollectionUtils.extractElementPropertyToString(list, "id", ","));
+	}
+
+	@Test
+	public void convertElementPropertyToList() {
+		TestBean3 bean1 = new TestBean3();
+		bean1.setId(1);
+		TestBean3 bean2 = new TestBean3();
+		bean2.setId(2);
+
+		List list = Lists.newArrayList(bean1, bean2);
+		List<String> result = CollectionUtils.extractElementPropertyToList(list, "id");
+		assertEquals(2, result.size());
+		assertEquals(1, result.get(0));
+	}
+
+}
