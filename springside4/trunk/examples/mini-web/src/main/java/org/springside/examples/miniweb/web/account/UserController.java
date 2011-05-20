6e1cc43a044788717d4515c60e51542d58e670c9
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2011-05-20 07:28:08 +0000

    little change

--- springside4/trunk/examples/mini-web/src/main/java/org/springside/examples/miniweb/web/account/UserController.java
+++ springside4/trunk/examples/mini-web/src/main/java/org/springside/examples/miniweb/web/account/UserController.java
@@ -1,31 +1,46 @@
 package org.springside.examples.miniweb.web.account;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springside.examples.miniweb.entity.account.Group;
 import org.springside.examples.miniweb.entity.account.User;
 import org.springside.examples.miniweb.service.account.AccountManager;
 
 @Controller
-@RequestMapping(value = "/account")
+@RequestMapping(value = "/account/user")
 public class UserController {
 
 	private AccountManager accountManager;
 
-	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public String input(@PathVariable Long id, Model model) {
-		User user;
-		if (id == null) {
-			user = new User();
-		} else {
-			user = accountManager.getUser(id);
-		}
+	@RequestMapping(value = { "list", "" })
+	public String list() {
+		return "account/userList";
+	}
 
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = accountManager.getUser(id);
 		model.addAttribute(user);
-		return "account/user-form";
+		return "account/userForm";
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.GET)
+	public String createForm(Model model) {
+		return "account/userForm";
+	}
+
+	@ModelAttribute("groups")
+	public Map<Long, String> getGroups() {
+		List<Group> allRoles = accountManager.getAllGroup();
+		return null;
 	}
 
 	@Autowired
