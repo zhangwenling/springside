af6f461f590653ae831e0bf21c94c7b6035a55eb
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2011-11-10 20:30:44 +0000

    升级Joda Demo

--- springside4/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/time/JodaDemo.java
+++ springside4/trunk/examples/showcase/src/main/java/org/springside/examples/showcase/time/JodaDemo.java
@@ -1,26 +1,48 @@
 package org.springside.examples.showcase.time;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Locale;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.Years;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.junit.Assert;
+import org.junit.Test;
 
 public class JodaDemo {
 
-	public static void main(String[] args) {
-		testTimeZone();
-		testLocale();
-		testGetAge();
+	@Test
+	public void convertToString() {
+		String format = "yyyy-MM-dd HH:mm:ss";
+		DateTime fooDate = new DateTime(1978, 6, 1, 12, 10, 8, 0);
+		//第一种方法  直接使用DateTime的toString方法 
+		System.out.println(fooDate.toString(format));
+
+		//第二种方法,使用Formatter
+		DateTimeFormatter fmt = DateTimeFormat.forPattern(format);
+		System.out.println(fmt.print(fooDate));
 	}
 
-	public static void test() {
+	@Test
+	public void convertFromString() {
+		String dateString = "1978-06-01 12:10:08";
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+		//第一种方法，直接构造函数,注意日期和时间之间用T分割
+		DateTime dt1 = new DateTime("1978-06-01");
+		assertEquals(1978, dt1.getYear());
+		DateTime dt2 = new DateTime("1978-06-01T12:10:08");
+		assertEquals(1978, dt2.getYear());
+
+		//第二种方法，使用Formatter
+		DateTime dt3 = fmt.parseDateTime(dateString);
+		assertEquals(1978, dt3.getYear());
 
 	}
 
-	public static void testTimeZone() {
+	@Test
+	public void timeZone() {
 
 		System.out.println("演示时区");
 
@@ -42,29 +64,33 @@ public class JodaDemo {
 		DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 
 		DateTime parserResult1 = fmt.withZone(DateTimeZone.forID("US/Pacific")).parseDateTime(dateString);
-		DateTime parserResult2 = fmt.withZone(DateTimeZone.UTC).parseDateTime(dateString);
+		DateTime parserResult2 = fmt.withZoneUTC().parseDateTime(dateString);
 
 		System.out.println(parserResult1.toString(format) + " " + parserResult1.getMillis());
 		System.out.println(parserResult2.toString(format) + " " + parserResult2.getMillis());
 	}
 
-	public static void testLocale() {
+	/**
+	 * 打印当地语言年，月，日到写法
+	 */
+	@Test
+	public void locale() {
 
 		System.out.println("演示Locale");
 
 		DateTime dateTime = new DateTime().withZone(DateTimeZone.UTC);
 
 		//打印中文与英文下不同长度的日期格式串
-		System.out.println("S:  " + DateUtils.formatDateTime(dateTime, "SS", "zh"));
-		System.out.println("M:  " + DateUtils.formatDateTime(dateTime, "MM", "zh"));
-		System.out.println("L:  " + DateUtils.formatDateTime(dateTime, "LL", "zh"));
-		System.out.println("XL: " + DateUtils.formatDateTime(dateTime, "FF", "zh"));
+		System.out.println("S:  " + formatDateTime(dateTime, "SS", "zh"));
+		System.out.println("M:  " + formatDateTime(dateTime, "MM", "zh"));
+		System.out.println("L:  " + formatDateTime(dateTime, "LL", "zh"));
+		System.out.println("XL: " + formatDateTime(dateTime, "FF", "zh"));
 		System.out.println("");
 
-		System.out.println("S:  " + DateUtils.formatDateTime(dateTime, "SS", "en"));
-		System.out.println("M:  " + DateUtils.formatDateTime(dateTime, "MM", "en"));
-		System.out.println("L:  " + DateUtils.formatDateTime(dateTime, "LL", "en"));
-		System.out.println("XL: " + DateUtils.formatDateTime(dateTime, "FF", "en"));
+		System.out.println("S:  " + formatDateTime(dateTime, "SS", "en"));
+		System.out.println("M:  " + formatDateTime(dateTime, "MM", "en"));
+		System.out.println("L:  " + formatDateTime(dateTime, "LL", "en"));
+		System.out.println("XL: " + formatDateTime(dateTime, "FF", "en"));
 		System.out.println("");
 		System.out.println("");
 
@@ -75,16 +101,40 @@ public class JodaDemo {
 		System.out.println("ML Mix: " + formatter.print(dateTime.getMillis()));
 
 		//只打印日期不打印时间
-		System.out.println("Date only :" + DateUtils.formatDateTime(dateTime, "M-", "zh"));
+		System.out.println("Date only :" + formatDateTime(dateTime, "M-", "zh"));
 
 	}
 
-	public static void testGetAge() {
-		DateTime now = new DateTime();
+	public static String formatDateTime(DateTime dateTime, String style, String lang) {
+		DateTimeFormatter formatter = DateTimeFormat.forStyle(style).withLocale(new Locale(lang));
+		return dateTime.toString(formatter);
+	}
 
-		DateTime oneYearsAgo = now.minusYears(2).plusDays(20);
-		DateTime twoYearsAgo = now.minusYears(2);
-		Assert.assertTrue(1 == DateUtils.getAge(oneYearsAgo));
-		Assert.assertTrue(2 == DateUtils.getAge(twoYearsAgo));
+	@Test
+	/**
+	 * 演示日期的加钱以及计算日期间的间隔，可使用任意时间单位进行加减和计算间隔.
+	 */
+	public void daysPlusAndMinusBetweenAndBetweenx() {
+		DateTime now = new DateTime();
+		DateTime birthDate = now.minusYears(10);
+		assertEquals(10, Years.yearsBetween(birthDate, new DateTime()).getYears());
+		birthDate = now.minusYears(10).plusDays(2);
+		assertEquals(9, Years.yearsBetween(birthDate, new DateTime()).getYears());
+	}
+
+	@Test
+	/**
+	 * 取得月份的头一天和最后一天.
+	 * 取得一天的0:00和23:59:59
+	 * 其他如年，星期的头一天，最后一天同理可证
+	 */
+	public void beginAndEndOfDates() {
+		String dateString = "1978-06-10T12:10:08";
+		DateTime dt = new DateTime(dateString);
+		DateTime startOfMonth = dt.dayOfMonth().withMinimumValue().withTimeAtStartOfDay();
+		System.out.println(startOfMonth.toString());
+
+		DateTime endOfMonth = dt.dayOfMonth().withMaximumValue().millisOfDay().withMaximumValue();
+		System.out.println(endOfMonth);
 	}
 }
