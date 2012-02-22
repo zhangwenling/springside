12f78f04330a9e3ecad62b784b5f78fc9719c8d9
calvinxiu <calvinxiu@fa9a2031-a219-0410-832a-cdfdc9972eb7>
2012-02-22 05:14:15 +0000

    Spring MVC 艰难使用中。

--- springside4/trunk/examples/mini-web/src/main/java/org/springside/examples/miniweb/web/account/GroupListEditor.java
+++ springside4/trunk/examples/mini-web/src/main/java/org/springside/examples/miniweb/web/account/GroupListEditor.java
@@ -5,16 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springside.examples.miniweb.entity.account.Group;
 import org.springside.examples.miniweb.service.account.AccountManager;
 import org.springside.modules.utils.Collections3;
 
+@Component
 public class GroupListEditor extends PropertyEditorSupport {
 	private AccountManager accountManager;
-
-	public void GroupListEditor(AccountManager accountManager) {
-		this.accountManager = accountManager;
-	}
 
 	public void setAsText(String text) throws IllegalArgumentException {
 		String[] ids = StringUtils.split(text, ",");
@@ -28,6 +27,11 @@ public class GroupListEditor extends PropertyEditorSupport {
 	@Override
 	public String getAsText() {
 		return Collections3.extractToString((List) getValue(), "id", ",");
+	}
+
+	@Autowired
+	public void setAccountManager(AccountManager accountManager) {
+		this.accountManager = accountManager;
 	}
 
 }
